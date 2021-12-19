c6e70f91df48cdd14f9f0f7771bd95ec8ae8fa79
xTrimy <Mohamed.ashraf881999@gmail.com>
2021-12-19 17:32:24 +0200

    CSS Production

--- resources/views/unsubscribeauto.blade.php
+++ resources/views/unsubscribeauto.blade.php
@@ -6,7 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Unsubscribe</title>
-    <link rel="stylesheet" href="./assets/css/tailwind.output.css" />
     <link
       href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
       rel="stylesheet"
